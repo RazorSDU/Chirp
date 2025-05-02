5b654d2ab722ec61515d15cb0aec06b0bc5b29a9
MJR-Git <mayarexen@protonmail.com>
2025-05-02 09:50:54 +0200

    added user service and interface

---
+++ Chirp.Core/Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,8 @@
+namespace Chirp.Core.Domain.Interfaces.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<UserDto>> GetAllUsersAsync();
+    Task<IEnumerable<UserDto>> GetUsersByIdAsync(int userId);
+    Task<UserDto> CreateUserAsync(CreateUserDto createUserDto);
+}
