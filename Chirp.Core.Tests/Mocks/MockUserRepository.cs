2b5593c0876b7a7aa5d60379aedb2d418b4c62b9
MJR-Git <mayarexen@protonmail.com>
2025-05-02 13:40:23 +0200

    added unit tests for userservice

---
+++ Chirp.Core.Tests/Mocks/MockUserRepository.cs
@@ -0,0 +1,33 @@
+using Chirp.Core.Domain.Entities;
+using Chirp.Core.Domain.Interfaces.Repositories;
+
+namespace Chirp.Tests.Backend.Core.Mocks;
+
+public class MockUserRepository : IUserRepository
+{
+    private readonly List<User> _usertable = new List<User>();
+    public async Task SaveUserToDatabaseAsync(User user)
+    {
+        _usertable.Add(user);
+    }
+
+    public async Task<User> SaveUpdatedUserToDatabaseAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteUserFromDatabaseAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> GetUserFromDatabaseByIdAsync(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsersFromDatabaseAsync()
+    {
+        return _usertable;
+    }
+}
