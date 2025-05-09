d53549f8d54ea9422ad656068d899b8546864a27
Quill VJ <129004429+Quill-Bookwyrm@users.noreply.github.com>
2025-05-09 11:55:01 +0200

    critical bugfix relating to imports in loginform - copy

--- chirp.frontend/src/components/LoginForm/LoginForm - Copy.jsx
+++ chirp.frontend/src/components/LoginForm/LoginForm - Copy.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import React from "react";
-import * as Input from '../../../node_modules/postcss/lib/input';
+import { useState } from 'react';
 
 export default function LoginForm() {
     const [username, setUsername] = useState('Username');
